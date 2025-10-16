8d850a41560a7f0d80f6c0fa5e9d2523f020d50a
Emilia Ivanova <emiliai96@yahoo.com>
2025-10-16 20:21:38 +0100

    interview test

--- Tennis/TennisGame.cs
+++ Tennis/TennisGame.cs
@@ -1,81 +1,65 @@
-namespace Tennis
-{
-    public class TennisGame : ITennisGame
-    {
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Tennis {
+    public class TennisGame : ITennisGame {
         private int m_score1 = 0;
         private int m_score2 = 0;
         private string player1Name;
         private string player2Name;
 
-        public TennisGame(string player1Name, string player2Name)
-        {
+        public TennisGame(string player1Name, string player2Name) {
             this.player1Name = player1Name;
             this.player2Name = player2Name;
         }
 
-        public void WonPoint(string playerName)
-        {
-            if (playerName == "player1")
+        public void WonPoint(string playerName) {
+            if (playerName == this.player1Name)
                 m_score1 += 1;
             else
                 m_score2 += 1;
         }
 
-        public string GetScore()
-        {
-            string score = "";
-            var tempScore = 0;
-            if (m_score1 == m_score2)
-            {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+        public string GetScore() {
+            if (m_score1 == m_score2) {
+                return GetEvenScore(m_score1);
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
-            {
+            else if (m_score1 >= 4 || m_score2 >= 4) {
                 var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
-            }
-            else
-            {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
+                var difference = Math.Abs(m_score1 - m_score2);
+                var leadPlayer = m_score1 > m_score2 ? player1Name : player2Name;
+
+                if (difference == 1) {
+                    return $"Advantage {leadPlayer}";
+                }
+                else {
+                    return $"Win for {leadPlayer}";
                 }
             }
-            return score;
+            else {
+                var playerOne = GetTempScoring(m_score1);
+                var playerTwo = GetTempScoring(m_score2);
+                return $"{playerOne}-{playerTwo}";
+            }
+        }
+
+        private string GetTempScoring(int playerScore) {
+            return playerScore switch {
+                0 => "Love",
+                1 => "Fifteen",
+                2 => "Thirty",
+                3 => "Forty",
+                _ => throw new System.Exception("Invalid score")
+            };
+        }
+
+        private string GetEvenScore(int playerScore) {
+            return playerScore switch {
+                0 => "Love-All",
+                1 => "Fifteen-All",
+                2 => "Thirty-All",
+                _ => "Deuce"
+            };
         }
     }
 }
